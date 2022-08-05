da8fce58f2c2b7bd072014befc52f2b529f48ff2
jamal <lordareello@gmail.com>
2022-08-05 18:28:59 +0100

    changes to main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.api.v1.api import router as api_router
-import uvicorn
 
 
 
@@ -9,13 +8,24 @@ import uvicorn
 app = FastAPI()
 
 
+TITLE = 'Jina NOW'
+DESCRIPTION = 'The Jina NOW service API'
+AUTHOR = 'Jina AI'
+EMAIL = 'hello@jina.ai'
+__version__ = 'latest'
 
 
 
 @app.get('/')
 async def root():
-    return {"message": "Hello World"}
-
+    #return {"message": "Hello World"}
+    return {
+        "title": TITLE,
+        "description": DESCRIPTION,
+        "author": AUTHOR,
+        "email": EMAIL,
+        "version": __version__,
+    }
 
 app.include_router(api_router, prefix='/api/v1')
 
